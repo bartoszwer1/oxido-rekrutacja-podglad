4591f29c98dbee64cbea9c9a4190b392a8ce8183
Bartosz Werminski <bartosz@werminski.pl>
2024-11-13 20:27:15 +0100

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -12,3 +12,16 @@ navLinks.addEventListener('click', (event) => {
         navLinks.classList.remove('open');
     }
 });
+
+// Dodaj nasłuchiwanie na wszystkie linki kotwiczne
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
